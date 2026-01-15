61bf0495ee03a7ba69684b4aa5b63e44212ae68a
Veronica Paul <veron.j.paul@gmail.com>
2026-01-15 14:05:20 +1100

    Add data check queries for tables and previews
    Added SQL queries to list tables and preview data.

--- queries/01_data_check.sql
+++ queries/01_data_check.sql
@@ -1,1 +1,7 @@
+-- list tables (works in many SQLite setups; if not, use your UI table browser)
+SELECT name FROM sqlite_master WHERE type='table';
 
+-- preview
+SELECT * FROM orders LIMIT 5;
+SELECT * FROM customers LIMIT 5;
+SELECT * FROM order_payments LIMIT 5;
